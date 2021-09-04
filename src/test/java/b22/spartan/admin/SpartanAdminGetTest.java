f2397fff8a3d869ca0af35c8912716a0217da424
asuman <ozarasuman@gmail.com>
2021-09-04 11:23:45 -0400

    Initial commit

---
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -0,0 +1,40 @@
+package b22.spartan.admin;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SerenityTest
+public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://44.195.19.167:7000";
+
+    }
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+
+    }
+}
